8f4ebd5de246b234c40ce784861d60cdce3b8665
Zac1017 <9964262@bedfordnhk12.net>
2026-09-06 20:41:13 -0400

    Add Calendar script to display a month with a highlighted target day

---
+++ src/Calendar.py
@@ -0,0 +1,15 @@
+import calendar
+
+year = int(input("Enter year: "))
+month = int(input("Enter month: "))
+day = int(input("Enter day: "))
+
+string = calendar.month(year, month)
+
+highlight = "\033[30;43m"  # Black text on yellow background]"
+
+target_day_with_space = f" {day:2d} "
+
+if target_day_with_space in string:
+    highlighted_string = string.replace(target_day_with_space, f"{highlight}{target_day_with_space}\033[0m")
+    print(highlighted_string)
